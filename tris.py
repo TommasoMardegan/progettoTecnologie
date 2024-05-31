5d51ac9520c6966293ee77590f0ed2f07bd4cd9e
TommasoMardegan <amardegan908@gmail.com>
2024-05-31 23:20:39 +0200

    aggiunta class agente

--- tris.py
+++ tris.py
@@ -34,6 +34,44 @@ class Utente:
             except:
                 print("Please enter valid move")
 
+class Agente:
+    def __init__(self, eps=0.1, alpha=0.5):
+        self.eps = eps  
+        self.alpha = alpha  # velocità di apprendimento
+        self.state_history = []
+
+    def setV(self, V):
+        self.V = V
+
+    def set_symbol(self, symbol):
+        self.symbol = symbol
+
+    def reset_history(self):
+        self.state_history = []
+
+    def take_action(self, env):
+        if np.random.rand() < self.eps:
+            # azione random
+            empty_moves = env.get_empty_moves()
+            index = np.random.choice(len(empty_moves))
+            next_move = empty_moves[index]
+        else:
+            next_move, _ = env.get_next_best_move(self)
+        env.board[next_move[0], next_move[1]] = self.symbol
+
+    def update_state_history(self, s):
+        self.state_history.append(s)
+
+    def update(self, env):
+        reward = env.reward(self.symbol)
+        target = reward
+        for prev in reversed(self.state_history):
+            value = self.V[prev] + self.alpha * (target - self.V[prev])
+            self.V[prev] = value
+            target = value
+        self.reset_history()
+
+
 def gioca():
     print("")
 
